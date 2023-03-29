590795baf47a7b171074f15f3dea7b1a0fbb0eb2
Pdtcode <trinhpeter15@gmail.com>
2023-03-28 21:37:05 -0500

    0.02.3
    -fixed store item captions
    -add prices

--- src/component/Store.jsx
+++ src/component/Store.jsx
@@ -11,28 +11,33 @@ import StoreImg from './StoreImg'
 const Store = () => {
   return (
     
-    <div className='max-w-[1240px] mx-auto text-center'>
+    <div className='max-w-[1240px] mx-auto text-center logo'>
         <p className='text-2xl font-bold'>Beans</p>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4'>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4 logo'>
             <div>
             <StoreImg socialImg={CostaRica}/>
             <p>Costa Rica</p>
+            <p className='caveat'>22</p>
             </div>
             <div>
             <StoreImg socialImg={Decaf}/>
-            <p>Cold Brew</p>
+            <p>Decaf</p>
+            <p className='caveat'>20</p>
             </div>
             <div>
             <StoreImg socialImg={Ethiopia1}/>
-            <p>Espresso</p>
+            <p>Natural Ethiopia</p>
+            <p className='caveat'>22</p>
             </div>
             <div>
             <StoreImg socialImg={Ethiopia2}/>
-            <p>Flat White</p>
+            <p>Ethiopia</p>
+            <p className='caveat'>22</p>
             </div>
             <div>
             <StoreImg socialImg={Guatemala}/>
-            <p>Hot Chocolate</p>
+            <p>Guatemala</p>
+            <p className='caveat'>17</p>
             </div>
         </div>
     </div>
