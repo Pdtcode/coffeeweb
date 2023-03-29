5d8534d0ad6efa4e17e9c18c526350c599593535
Pdtcode <trinhpeter15@gmail.com>
2023-03-28 21:46:10 -0500

    Update Contact.jsx
    -minor changes to contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -53,34 +53,33 @@ export default function Contact() {
       {/*Overlay For Mobile*/}      
       <div className='flex items-center justify-center bg-inherit bg-center bg-cover contact-bgimg h-screen sm:hidden'>
         <div className='absolute top-0 left-0 h-screen right-0 bottom-0 bg-black/60 z-[2]'/>
-          <div className='z-[3] mb-[5rem] mt-[9rem]'>
-            <div className=' text-white mb-[2rem] ml-[2.5rem] mr-[2.5rem]'>
-              <h2 className='text-2xl font-bold'>Contact</h2>
-              <p className=' text-[.6rem] text-yellow-400'>*still need to connect the send button</p>
+          <div className='z-[3] mb-[1rem] mt-[2rem]'>
+            <div className=' text-white ml-[2.5rem] mr-[2.5rem]'>
+              <h2 className='text-lg font-bold'>Contact</h2>
             </div>
 
       {/*Mobile Form*/}      
-      <form /*onSubmit={handleSubmit}*/ className="rounded-3xl shadow-xl sm:flex flex-col px-[30px] py-[20px] bg-amber-800/40 z-[3] ml-[2rem] mr-[2rem]">
+      <form /*onSubmit={handleSubmit}*/ className="rounded-3xl shadow-xl sm:flex flex-col px-[1rem] py-[1rem] bg-amber-800/40 z-[3] ml-[2rem] mr-[2rem]">
 
         {/**Full Name */}
         <label for="fullname" className="flex text-gray-500 font-light dark:text-gray-50 pr-10">Full Name 2<span className="text-red-500">*</span></label>
-        <input type="text" value={fullname} onChange={(e) => {setFullname(e.target.value);}} name="fullname" className="bg-transparent border-b py-3 pl-2 pr-1 focus:outline-none focus:rounded-md focus:ring-1 ring-amber-700/40 font-light text-gray-50 mb-4" />
+        <input type="text" value={fullname} onChange={(e) => {setFullname(e.target.value);}} name="fullname" className="bg-transparent border-b py-2 pl-2 pr-1 focus:outline-none focus:rounded-md focus:ring-1 ring-amber-700/40 font-light text-gray-50 mb-4" />
 
         {/**Email */}
         <label for="email" className="flex text-gray-50 font-light">E-mail<span className="text-red-500">*</span></label>
-        <input  type="email" name="email" value={email} onChange={(e) => {setEmail(e.target.value);}} className="bg-transparent border-b py-3 pl-2 pr-1 focus:outline-none focus:rounded-md focus:ring-1 ring-amber-700/40 font-light text-gray-50 mb-4" />
+        <input  type="email" name="email" value={email} onChange={(e) => {setEmail(e.target.value);}} className="bg-transparent border-b py-2 pl-2 pr-1 focus:outline-none focus:rounded-md focus:ring-1 ring-amber-700/40 font-light text-gray-50 mb-4" />
         
         {/**Subject */}
         <label for="subject" className="flex text-gray-500 font-light dark:text-gray-50">Subject<span className="text-red-500">*</span></label>
-        <input type="text" name="subject" value={subject} onChange={(e) => { setSubject(e.target.value); }} className="bg-transparent border-b py-3 pl-2 pr-1 focus:outline-none focus:rounded-md focus:ring-1 ring-amber-700/40 font-light text-gray-50 mb-4" />
+        <input type="text" name="subject" value={subject} onChange={(e) => { setSubject(e.target.value); }} className="bg-transparent border-b py-2 pl-2 pr-1 focus:outline-none focus:rounded-md focus:ring-1 ring-amber-700/40 font-light text-gray-50 mb-4" />
 
         {/**Message */}
         <label for="message" className="flex text-gray-500 font-light dark:text-gray-50">Message<span className="text-red-500">*</span></label>
-        <textarea name="message" value={message} onChange={(e) => { setMessage(e.target.value); }} className="bg-transparent border-b py-3 pr-8 pl-2 focus:outline-none focus:rounded-md focus:ring-1 ring-amber-700/40 font-light text-gray-50 mb-4"></textarea>
+        <textarea name="message" value={message} onChange={(e) => { setMessage(e.target.value); }} className="bg-transparent border-b py-2 pr-8 pl-2 focus:outline-none focus:rounded-md focus:ring-1 ring-amber-700/40 font-light text-gray-50 mb-2"></textarea>
 
         {/**Send */}
         <div className="flex flex-row items-center justify-start">
-          <button type='submit' className="px-10 mt-2 py-2 bg-amber-800/60 text-gray-50 font-light rounded-md text-lg flex flex-row items-center raleway">
+          <button type='submit' className="px-10 py-2 bg-amber-800/60 text-gray-50 font-light rounded-md text-lg flex flex-row items-center raleway">
             Send
           </button>
         </div>
