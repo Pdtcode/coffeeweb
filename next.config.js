5e94651987b1dcf3ac2be6e6e9f10c8d80e09e40
Pdtcode <trinhpeter15@gmail.com>
2023-04-02 17:53:21 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+
 
 module.exports = {
   images: {
@@ -13,14 +10,4 @@ module.exports = {
     // This ensures that the server-side version of the nodemailer module is used
     externals: ['nodemailer'],
   },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    return config
-  }
 }
