08256d9c83a988ee88c04cd41f4ccc220b32c4eb
Pdtcode <trinhpeter15@gmail.com>
2023-04-03 12:48:30 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,4 @@ module.exports = {
       'images.unsplash.com'
     ]
   },
-  server: {
-    // This ensures that the server-side version of the nodemailer module is used
-    externals: ['nodemailer'],
-  },
 }
